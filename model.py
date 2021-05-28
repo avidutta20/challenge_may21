216f821e8be3b3769fede0235bb648a6ebefd5b9
avidutta <avishek.dutta91@gmail.com>
2021-05-28 21:44:30 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,31 @@
-import sklearn
+# -*- coding: utf-8 -*-
+"""
+OM NAMAH SHIVAY_/\_
+JOY MAA TARAR JOY
+
+Created on Fri May 28 19:53:04 2021
+# Job a thon in NLP
+# MAY 2021
+@author: RAJA
+"""
+import pandas as pd
+import numpy as np
+import os
+cwd = 'D:\\development\\git\\jobathon_may21\\'
+os.chdir(cwd)
+print(f'CWD changed to {cwd} successfully')
+
+train_data_path = 'dataset\\av_jobathon_may21_train.csv'
+test_data_path = 'dataset\\av_jobathon_may21_test.csv'
+sample_sub_path = 'dataset\\av_jobathon_may21_sample_submisssion.csv'
+
+data_ = pd.read_csv(train_data_path)
+test_df = pd.read_csv('')
+
+
+# =============================================================================
+'''
+NAMAH SHIVAY _/\_
+'''
+# =============================================================================
+
